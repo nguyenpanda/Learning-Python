790f95155c183ae0cf913093f726af5a8a5caaf9
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-02-18 02:06:04 +0700

    Commit

--- Test.py/Test0.py
+++ Test.py/Test0.py
@@ -1,11 +1,14 @@
-# from setuptools import setup, find_packages
-#
-# setup(
-#     name='myprojectname',
-#     version='1.0',
-#     packages=find_packages()
-# )
 
-# python setup.py built bdist_wheel
-# rd /s /q built
-# rd /s /q dist
+import functools
+
+@functools.lru_cache(maxsize=1000)
+def fibonacci(n: int) -> int:
+    if n == 0 or n == 1:
+        return 1
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+file = open('text.txt', 'a')
+for i in range(1000):
+    file.writelines(str(fibonacci(i)) + '\n')
